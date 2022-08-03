d73c836acba14bc0652fe876c0d756a8d03fd57f
Michael Clifford <cliffom@gmail.com>
2022-08-03 11:03:04 -0700

    Some movement helpers

--- entity_bee.go
+++ entity_bee.go
@@ -15,46 +15,61 @@ type Bee struct {
 	Sprite
 }
 
+// Draw returns the rune that represents the current state of our Bee
 func (b *Bee) Draw() rune {
 	return b.frames[b.frame]
 }
 
-func (b *Bee) Move(dir int, e Entity) bool {
-	b.frame = dir
+// Move checks for an entity in what would be our Bee's occupying
+// cell post-movement. If the cell-to-be-occupied has no occupying
+// entity, update the Bee's position based on movement vectors.
+func (b *Bee) Move(d int, e Entity) bool {
+	b.frame = d
 
-	if e == nil {
-		switch dir {
-		case UP:
-			b.vx = 0
-			b.vy = -1
-		case RIGHT:
-			b.vx = 1
-			b.vy = 0
-		case DOWN:
-			b.vx = 0
-			b.vy = 1
-		case LEFT:
-			b.vx = -1
-			b.vy = 0
-		}
-
-		b.x += b.vx
-		b.y += b.vy
-
-		return true
+	if e != nil {
+		return false
 	}
 
-	return false
+	b.UpdatePosition(d)
+	return true
+
 }
 
+// GetPosition returns the current (x, y) coordinates of a Bee
 func (b *Bee) GetPosition() (x, y int) {
 	return b.x, b.y
 }
 
+// UpdatePosition updates a Bee's velocity (vx, vy) and position (x, y)
+// based on an intended direction
+func (b *Bee) UpdatePosition(d int) {
+	switch d {
+	case UP:
+		b.SetVelocity(0, -1)
+	case RIGHT:
+		b.SetVelocity(1, 0)
+	case DOWN:
+		b.SetVelocity(0, 1)
+	case LEFT:
+		b.SetVelocity(-1, 0)
+	}
+
+	b.x += b.vx
+	b.y += b.vy
+}
+
+// SetVelocity sets a Bee's velocity to the incoming (vx, vy)
+func (b *Bee) SetVelocity(vx, vy int) {
+	b.vx = vx
+	b.vy = vy
+}
+
+// GetColor returns the current color of a Bee
 func (b *Bee) GetColor() tcell.Color {
 	return b.color
 }
 
+// NewBee returns a new Bee
 func NewBee() *Bee {
 	return &Bee{
 		Position: Position{
